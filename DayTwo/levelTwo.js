20a5c4fb737bd3a9830eebb4b36b4e5b0b6088b4
Obinna Victor <nkirevictor77@gmail.com>
2023-02-14 19:35:42 +0100

    level two

---
+++ DayTwo/levelTwo.js
@@ -0,0 +1,13 @@
+/*1*/ const firstStatement = 'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another';
+console.log(firstStatement);
+
+/*2*/const secondStatement =
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+  console.log(secondStatement)
+
+/*3*/let stringTen = "10";
+console.log(stringTen === 10)
+stringTen = +stringTen
+console.log(stringTen === 10);
+
+/*4*/console.log(parseFloat("9.8"))
